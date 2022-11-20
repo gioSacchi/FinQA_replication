ee14cd4103560cbfdbf722d77a848cf98b3203fe
Alexander Brokking <alexander.balfors@gmail.com>
2022-11-20 20:27:06 +0100

    Added resume model to main of generator

--- code/generator/Main.py
+++ code/generator/Main.py
@@ -125,6 +125,8 @@ def train():
 
     model = nn.DataParallel(model)
     model.to(conf.device)
+    if conf.resume_model_path:
+        model.load_state_dict(torch.load(conf.resume_model_path))
     optimizer = optim.Adam(model.parameters(), conf.learning_rate)
     criterion = nn.CrossEntropyLoss(reduction='none', ignore_index=-1)
     model.train()
