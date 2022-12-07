c86c6f1d83c973fbae411e4342f4ff571735cfea
pingu <giorgiosacchi@me.com>
2022-12-07 19:55:31 +0100

    small fixa and prel sketch of WSD

--- code/pipeline/syn_rep.py
+++ code/pipeline/syn_rep.py
@@ -90,7 +90,7 @@ def convert_to_wn_pos(pos):
 
 def new_text_naive(text: str, threshold: float, less_naive = False):
     # find all words
-    words = re.findall(r'\b[^\d\W]+\b', text) # taking into account ' as in don't???
+    words = re.findall(r'\b[^\d\W]+\b', text) # taking into account ' as in don't??? This maybe works \b[a-zA-Z]+(?:\'[a-zA-Z]+)?\b
 
     new_text = text
     changes = {}
@@ -171,6 +171,65 @@ def naive_synonym_replacement(row, df_index, less_naive = False):
             new_row['post_text'][row_index-break_point] = sequence_good_replace(new_row['post_text'][row_index-break_point], changes)
 
     # update question with synonyms
+    new_question, changes = new_text_naive(question, threshold, less_naive)
+    new_row["qa"]["question"] = new_question
+    total_old += question
+    total_new += new_question
+
+    if total_new == total_old:
+        # Don't create new question
+        return None
+
+    ## Update id TODO: Move to main function
+    new_row['id'] = new_row['id'] + "_augmented_" + str(df_index)
+
+    return new_row
+
+def synonym_replacement_WSD(row, df_index):
+    # copy of row
+    new_row = deepcopy(row.to_dict())
+    qa = new_row['qa']
+
+    # taking out the question and gold_inds
+    # do we want to change with questions too?
+    question = qa["question"]
+    gold_inds = qa["gold_inds"]
+
+    # index of first row in post_text
+    break_point = len(new_row['pre_text'])
+
+    # randomly setting threshold
+    threshold = random.random() # maybe one per threshold per thing?
+
+    # to check if any changes have been made
+    total_old = ""
+    total_new = ""
+
+    # update gold_inds and corresponding texts or tabels with new synonyms
+    for key, value in gold_inds.items():
+        new_text, changes = new_text_naive(value, threshold)
+        new_row['qa']['gold_inds'][key] = new_text
+
+        total_old += value + " "
+        total_new += new_text + " "
+
+        if changes == None:
+            # no need to update anything
+            continue
+
+        # Updating pre or post text and table with new synonym
+        parsed_key = key.split("_")
+        row_index = int(parsed_key[1])
+        if parsed_key[0] == "table":
+          for col_index, table_col in enumerate(new_row['table'][row_index]):
+            new_row['table'][row_index][col_index] = sequence_good_replace(table_col, changes) 
+        else:
+          if row_index < break_point:
+            new_row['pre_text'][row_index] = sequence_good_replace(new_row['pre_text'][row_index], changes)
+          else:
+            new_row['post_text'][row_index-break_point] = sequence_good_replace(new_row['post_text'][row_index-break_point], changes)
+
+    # update question with synonyms
     new_question, changes = new_text_naive(question, threshold)
     new_row["qa"]["question"] = new_question
     total_old += question
@@ -185,10 +244,60 @@ def naive_synonym_replacement(row, df_index, less_naive = False):
 
     return new_row
 
-def get_word_universal_tags(words: list):
-    # get tags for each word
-    words_w_tags = pos_tag(words,  tagset='universal')
+def WSD_new_text(text, threshold):
+    # find all words
+    words = re.findall(r'\b[^\d\W]+\b', text) # taking into account ' as in don't??? This maybe works \b[a-zA-Z]+(?:\'[a-zA-Z]+)?\b
 
+    new_text = text
+    changes = {}
+
+    # find unique words not included in stop_words
+    word_list = []
+    # This tagset is used in the WSD model, that's why we use it
+    words_with_tags = pos_tag(words,  tagset='universal')    
+    for word, tag in words_with_tags:
+        if word not in stop_words and word not in list(map(itemgetter(0), word_list)):
+            # do we want to add same word if different tag? If so substitution should be tag based too
+            word_list.append((word, tag))
+
+    # computing n from threshold and words and sample
+    n = math.ceil(len(word_list) * threshold)
+    random_word_list = random.sample(word_list, n)
+
+    # Replacing all occurences of selected words with random synonym.
+    for random_word in random_word_list:
+        synonyms = get_WSD_synonyms(random_word)
+        if len(synonyms) == 0:
+            continue
+        new_word = random.choice(synonyms)
+        if new_word in random_word_list:    
+            # here too should be necessary to look at random_word_list[i:]
+            continue
+        # print("replaced", random_word, "with", synonym)
+        new_text = good_replace(new_text, random_word, new_word)
+        changes[random_word] = new_word
+
+    return new_text, changes if changes else None
+
+def get_WSD_synonyms(words_w_tags, target):
+    word, tag = target
+    # make a list of all the synonyms (lemmas) to word 
+    # from all of it's possible meanings (all wordnet.synsets)
+    synonyms = set()
+    meaning = WSD(words_w_tags, word, tag)
+    syn = wn.synset(meaning)
+    for l in syn.lemmas(): 
+        synonym = l.name().replace("_", " ").replace("-", " ").lower()
+        synonym = "".join([char for char in synonym if char in ' qwertyuiopasdfghjklzxcvbnm'])
+        synonyms.add(synonym) 
+    if word in synonyms:
+        synonyms.remove(word)
+    return list(synonyms)
+
+def WSD(words_w_tags, word, tag):
+    """performes wsd using model
+        look at code and bring in right stuff"""
+    pass
 
 def main():
   input_path = r"C:\Users\pingu\FinQA_replication\dataset\train.json"
@@ -212,7 +321,10 @@ def main():
   df.to_json(output_path, orient='records', indent=4)
 
 
-
+"""problem in general is that it is hard to get correct form of word. eg changes 
+    gives synset change which has lemma alter which will replace changes. But correct 
+    would be plural form of alter alterations. How does one do that? Put in to a 
+    grammar check. Necessary? Maybe adds noise which is good anyways"""
 
 if __name__ == '__main__':
   main()
