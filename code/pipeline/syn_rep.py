9d5849df710aefd56eb7e787d496fa20b39e3898
pingu <giorgiosacchi@me.com>
2022-12-07 11:50:23 +0100

    added less naive with wordnet pos tags

--- code/pipeline/syn_rep.py
+++ code/pipeline/syn_rep.py
@@ -5,24 +5,26 @@ import re
 from copy import deepcopy
 import math
 from utils import *
+from operator import itemgetter
 
 import pandas as pd
-import re
-
 ## for the first time you use wordnet
 import nltk
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('universal_tagset')
+nltk.download('stopwords')
 from nltk.corpus import wordnet as wn
 from nltk import pos_tag
-# from nltk.tag import pos_tag
+from nltk.corpus import stopwords
+
 
 
 random.seed(1)
 
-#stop words list
+stop_words = set(stopwords.words('english'))
+# stop words list
 stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 
 			'ours', 'ourselves', 'you', 'your', 'yours', 
 			'yourself', 'yourselves', 'he', 'him', 'his', 
@@ -54,17 +56,18 @@ def get_synonyms_naive(word):
             synonym = l.name().replace("_", " ").replace("-", " ").lower()
             print(synonym)
             synonym = "".join([char for char in synonym if char in ' qwertyuiopasdfghjklzxcvbnm']) # sub with regex, Why do this??
-            # thousends -> 1000 , which becomes "". Is it ok to have numbers?
+            ###### thousends -> 1000 , which becomes "". Is it ok to have numbers?
             synonyms.add(synonym) 
     if word in synonyms: 
         synonyms.remove(word)
     return list(synonyms)
 
-def get_synonyms_less_naive(word):
+def get_synonyms_less_naive(word_w_tag):
+    word, tag = word_w_tag
     # make a list of all the synonyms (lemmas) to word 
     # from all of it's possible meanings (all wordnet.synsets)
     synonyms = set()
-    for syn in wn.synsets(word): 
+    for syn in wn.synsets(word, pos=convert_to_wn_pos(tag)): 
         for l in syn.lemmas(): 
             synonym = l.name().replace("_", " ").replace("-", " ").lower()
             synonym = "".join([char for char in synonym if char in ' qwertyuiopasdfghjklzxcvbnm'])
@@ -73,16 +76,35 @@ def get_synonyms_less_naive(word):
         synonyms.remove(word)
     return list(synonyms)
 
+def convert_to_wn_pos(pos):
+    if pos.startswith("J"):
+        return wn.ADJ
+    elif pos.startswith("V"):
+        return wn.VERB
+    elif pos.startswith("N"):
+        return wn.NOUN
+    elif pos.startswith("R"):
+        return wn.ADV
+    else:
+        return "" 
+
 def new_text_naive(text: str, threshold: float, less_naive = False):
     # find all words
-    words = re.findall(r'\b[^\d\W]+\b', text)
+    words = re.findall(r'\b[^\d\W]+\b', text) # taking into account ' as in don't???
 
     new_text = text
     changes = {}
 
     # find unique words not included in stop_words
-    # words_with_tags = pos_tag(words)
-    word_list = list(set([word for word in words if word not in stop_words]))
+    if less_naive:
+        word_list = []
+        words_with_tags = pos_tag(words)
+        for word, tag in words_with_tags:
+            if word not in stop_words and word not in list(map(itemgetter(0), word_list)):
+                 # do we want to add same word if different tag? If so substitution should be tag based too
+                word_list.append((word, tag))
+    else:
+        word_list = list(set([word for word in words if word not in stop_words]))
 
     # computing n from threshold and words and sample
     n = math.ceil(len(word_list) * threshold)
@@ -90,11 +112,12 @@ def new_text_naive(text: str, threshold: float, less_naive = False):
 
     # Replacing all occurences of selected words with random synonym.
     for random_word in random_word_list:
+        # Random_word will be tupple if less naive
         synonyms = get_synonyms_naive(random_word) if not less_naive else get_synonyms_less_naive(random_word)
         if len(synonyms) == 0:
             continue
         new_word = random.choice(synonyms)
-        if new_word in random_word_list:
+        if new_word in random_word_list:    
             # here too should be necessary to look at random_word_list[i:]
             continue
         # print("replaced", random_word, "with", synonym)
@@ -162,6 +185,35 @@ def naive_synonym_replacement(row, df_index, less_naive = False):
 
     return new_row
 
-def get_word_tags(words: list):
+def get_word_universal_tags(words: list):
     # get tags for each word
     words_w_tags = pos_tag(words,  tagset='universal')
+
+
+def main():
+  input_path = r"C:\Users\pingu\FinQA_replication\dataset\train.json"
+  df = pd.read_json(input_path)
+
+  print(len(df))
+
+  ## Remove retriever columns
+  df = df.drop(['table_retrieved','text_retrieved','table_retrieved_all','text_retrieved_all', 'table_ori', 'filename'], axis=1)
+
+  for df_index, row in df.iterrows():
+    # Dropping unneeded columns, remove program_re???
+    row['qa'] = {"question": row['qa']["question"], "program": row['qa']["program"], "gold_inds": row['qa']["gold_inds"], "exe_ans": row['qa']["exe_ans"], "program_re": row['qa']["program_re"]}
+  
+    row = naive_synonym_replacement(row, df_index, True)
+    if row:
+      df = pd.DataFrame.append(df, row, ignore_index=True)
+
+  print(len(df))
+  output_path = r"C:\Users\alexa\projects\su-kex\FinQA_replication\code\pipeline\output\train_augmented.json"
+  df.to_json(output_path, orient='records', indent=4)
+
+
+
+
+if __name__ == '__main__':
+  main()
+    
