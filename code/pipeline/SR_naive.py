950037f1fc77cde2bd9731cb50977309b1ee560d
gioSacchi <giorgiosacchi@me.com>
2022-12-10 01:01:50 +0100

    naive SR almost compleated

--- code/pipeline/SR_naive.py
+++ code/pipeline/SR_naive.py
@@ -5,15 +5,13 @@ import re
 from copy import deepcopy
 import math
 from utils import *
-from operator import itemgetter
-
 import pandas as pd
+
 ## for the first time you use wordnet
 import nltk
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('averaged_perceptron_tagger')
-nltk.download('universal_tagset')
 nltk.download('stopwords')
 from nltk.corpus import wordnet as wn
 from nltk import pos_tag
@@ -23,6 +21,20 @@ random.seed(1)
 
 stop_words = set(stopwords.words('english'))
 
+
+def get_synonyms_naive(word, tag = None, less_naive = False):
+    # make a list of all the synonyms (lemmas) to word 
+    # from all of it's possible meanings (all wordnet.synsets)
+    synonyms = set()
+    for syn in wn.synsets(word, pos=tag): 
+        for l in syn.lemmas(): 
+            synonym = l.name().replace("_", " ").replace("-", " ").lower()
+            # synonym = "".join([char for char in synonym if char in ' qwertyuiopasdfghjklzxcvbnm']) # sub with regex, Why do this??
+            ###### thousends -> 1000 , which becomes "". Is it ok to have numbers?
+            synonyms.add(synonym) 
+    if word in synonyms: 
+        synonyms.remove(word)
+    return list(synonyms)
 
 def naive_synonym_replacement(row, df_index, less_naive = False):
     # copy of row
@@ -43,6 +55,10 @@ def naive_synonym_replacement(row, df_index, less_naive = False):
 
     # update gold_inds and corresponding texts or tabels with new synonyms
     for key, text in gold_inds.items():
+        # skip if gold_ind is a table
+        if "table" in key:
+            continue
+
         # get all words in text
         words = re.findall(r'\b[a-zA-Z]+(?:\'[a-zA-Z]+)?\b', text)
 
@@ -51,7 +67,9 @@ def naive_synonym_replacement(row, df_index, less_naive = False):
 
         # get tags for words
         if less_naive:
-            words = pos_tag(words)
+          tags = []
+          for _, tag in pos_tag(words):
+            tags.append(tag)
 
         # randomly setting threshold
         threshold = random.random() 
@@ -59,42 +77,92 @@ def naive_synonym_replacement(row, df_index, less_naive = False):
         # computing n from threshold and choose which to sample
         n = math.ceil(len(words) * threshold)
         sample = random.sample(range(len(words)), n)
+        sampled_words = [words[index] for index in sample]
+
+        new_text = text
 
         for index in sample:
             word = words[index]
-            if less_naive:
-                word = word[0]
-                tag = word[1]
             # find alla instances of word in words and compute wich of them index corresponds to
             all_indices = [i for i, x in enumerate(words) if x == word]
             instance_of_selected = all_indices.index(index) + 1
 
+            # get synonyms
+            tag = tags[index] if less_naive else None
+            synonyms = get_synonyms_naive(word, tag, less_naive)
+            if len(synonyms) == 0:
+              continue
+            
+            # choose synonym
+            new_word = random.choice(synonyms)
+            # check that new word is not in sampled words (to avoid duplicates)
+            if new_word in sampled_words:
+              continue
+            
+            # replace word with new_word in text
+            new_text = replace_nth(new_text, word, new_word, instance_of_selected)
 
-        new_text, changes = new_text_naive(text, threshold, less_naive)
+        # update new_row
         new_row['qa']['gold_inds'][key] = new_text
+
+        # update pre and post text with new text
+        parsed_key = key.split("_")
+        row_index = int(parsed_key[1])
+        if row_index < break_point:
+          new_row['pre_text'][row_index] = new_text
+        else:
+          new_row['post_text'][row_index - break_point] = new_text
 
         total_old += text + " "
         total_new += new_text + " "
 
-        if changes == None:
-            # no need to update anything
-            continue
-
-        # Updating pre or post text and table with new synonym
-        parsed_key = key.split("_")
-        row_index = int(parsed_key[1])
-        if parsed_key[0] == "table":
-          for col_index, table_col in enumerate(new_row['table'][row_index]):
-            new_row['table'][row_index][col_index] = sequence_good_replace(table_col, changes) 
-        else:
-          if row_index < break_point:
-            new_row['pre_text'][row_index] = sequence_good_replace(new_row['pre_text'][row_index], changes)
-          else:
-            new_row['post_text'][row_index-break_point] = sequence_good_replace(new_row['post_text'][row_index-break_point], changes)
-
     # update question with synonyms
-    new_question, changes = new_text_naive(question, threshold, less_naive)
-    new_row["qa"]["question"] = new_question
+    # get all words in text
+    words = re.findall(r'\b[a-zA-Z]+(?:\'[a-zA-Z]+)?\b', question)
+
+    # sort out words that are in stop words
+    words = [word for word in words if word not in stop_words]
+
+    # get tags for words
+    if less_naive:
+      tags = []
+      for _, tag in pos_tag(words):
+        tags.append(tag)
+
+    # randomly setting threshold
+    threshold = random.random() 
+
+    # computing n from threshold and choose which to sample
+    n = math.ceil(len(words) * threshold)
+    sample = random.sample(range(len(words)), n)
+    sampled_words = [words[index] for index in sample]
+
+    new_question = question
+
+    for index in sample:
+        word = words[index]
+        # find alla instances of word in words and compute wich of them index corresponds to
+        all_indices = [i for i, x in enumerate(words) if x == word]
+        instance_of_selected = all_indices.index(index) + 1
+
+        # get synonyms
+        tag = tags[index] if less_naive else None
+        synonyms = get_synonyms_naive(word, tag, less_naive)
+        if len(synonyms) == 0:
+          continue
+        
+        # choose synonym
+        new_word = random.choice(synonyms)
+        # check that new word is not in sampled words (to avoid duplicates)
+        if new_word in sampled_words:
+          continue
+        
+        # replace word with new_word in text
+        new_text = replace_nth(new_text, word, new_word, instance_of_selected)
+
+    # update new_row
+    new_row['qa']['question'] = new_question
+
     total_old += question
     total_new += new_question
 
