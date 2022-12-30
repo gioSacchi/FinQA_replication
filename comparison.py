927365cc03cb7a9c43d42b21d9418791cec3fac8
gioSacchi <44093534+gioSacchi@users.noreply.github.com>
2022-12-30 13:56:04 +0100

    added prints, no inf, updated average calc

--- comparison.py
+++ comparison.py
@@ -1,6 +1,6 @@
 import nltk
-nltk.download('omw-1.4')
-nltk.download('stopwords')
+# nltk.download('omw-1.4')
+# nltk.download('stopwords')
 from nltk.corpus import stopwords
 from gensim.models import KeyedVectors
 from time import time
@@ -18,17 +18,18 @@ def tokenize(text):
 # Calculate the Word Mover's Distance between two sentences
 def compare_sentences(sentence1, sentence2, model):
     # Tokenize the sentences
-    sentence1_tokens = word_tokenize(sentence1.lower())
-    sentence2_tokens = word_tokenize(sentence2.lower())
+    sentence1_tokens = tokenize(sentence1.lower())
+    sentence2_tokens = tokenize(sentence2.lower())
 
     # Remove the stop words
     sentence1_tokens = [token for token in sentence1_tokens if token not in stop_words]
     sentence2_tokens = [token for token in sentence2_tokens if token not in stop_words]
 
     # Calculate the Word Mover's Distance
-    t1 = time()
+    # t1 = time()
     distance = model.wmdistance(sentence1_tokens, sentence2_tokens) 
-    print("Time taken to calculate WMD: ", time() - t1)
+    # print("Time taken to calculate WMD: ", time() - t1)
+    # print("WMD: ", distance)
 
     return distance
 
@@ -43,11 +44,17 @@ def main():
     df = pd.read_json(data_file)
 
     # load model
+    t1 = time()
     word2vec_path = r'C:\Users\pingu\Desktop\GoogleNews-vectors-negative300.bin.gz'
     model = KeyedVectors.load_word2vec_format(word2vec_path, binary=True)
+    print("Time taken to load model: ", time() - t1)
 
     # score for each class
     class_scores = dict((res_class, 0) for res_class in results_dict)
+
+    inf_count = 0
+
+    class_counts = dict((res_class, 0) for res_class in results_dict)
 
     # iterate over df and make comparisons
     for index, row in df.iterrows():
@@ -60,23 +67,39 @@ def main():
         golds = row["qa"]["gold_inds"]
 
         # make comparison and sum up distances
-        sum = 0
-        for text in golds.values():
+        avg = 0
+        count = 0
+        for key, text in golds.items():
+            # skip if table
+            if "table" in key:
+                continue
+
             # make comparison
-            sum += compare_sentences(question, text, model)
-        # compute avg
-        avg = sum / len(golds)
+            dist = compare_sentences(question, text, model)
+            # if inf add to inf_count 
+            if dist == float("inf"):
+                inf_count += 1
+            else:
+                # online average update
+                count += 1
+                avg = dist/count + avg*(count-1)/count
 
         # iterate over results and make comparisons
         for res_class, indecies in results_dict.items():
             # check if row id in indecies
-            if row["id"] in indecies:
+            if row["id"] in indecies and avg != float("inf") and count != 0:
                 # add average to class_scores
                 class_scores[res_class] += avg
+                class_counts[res_class] += 1
     
     # average class scores
     for res_class, score in class_scores.items():
-        class_scores[res_class] = score / len(results_dict[res_class])
+        class_scores[res_class] = score / class_counts[res_class]
+    
+    # print results
+    print("class_scores: ", class_scores)
+    print("class_counts: ", class_counts)
+    print("inf_count: ", inf_count)
 
 if __name__ == "__main__":
     main()
